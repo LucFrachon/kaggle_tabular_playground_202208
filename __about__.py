a9ef9d2a6c3f6ee444ef9389a3dac6b50a8a3caa
lucfrachon <luc.frachon@gmail.com>
2022-08-10 12:46:13 +0200

    First commit

---
+++ __about__.py
@@ -0,0 +1,17 @@
+__version__ = "0.0.0dev"
+__docs__ = "Package for Kaggle's 'Tabular Playground - August 2022' competition"
+__author__ = "Luc Frachon"
+__author_email__ = "luc.frachon@hw.ac.uk"
+__copyright__ = f"Copyright (c) 2020-2022, {__author__}."
+__homepage__ = "https://github.com/LucFrachon/kaggle_tabular_playground_202208"
+__license__ = "MIT"
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]
